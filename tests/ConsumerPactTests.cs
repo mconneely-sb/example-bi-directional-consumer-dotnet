8968fdad6705ed69c47a555a7a658b1d083dab55
mconneely-sb <84188442+mconneely-sb@users.noreply.github.com>
2025-03-07 14:32:41 +0000

    Update ConsumerPactTests.cs

--- tests/ConsumerPactTests.cs
+++ tests/ConsumerPactTests.cs
@@ -106,7 +106,7 @@ namespace tests
                 .UponReceiving("a request to retrieve a product id that does not exist")
                 .WithRequest(HttpMethod.Get, "/Products/10")
                 .WillRespond()
-                .WithStatus(System.Net.HttpStatusCode.NotFound)
+                .WithStatus(System.Net.HttpStatusCode.NotFound);
      
 
             //Act
