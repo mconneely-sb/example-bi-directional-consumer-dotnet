65e1b208031fd631783aaa8915b9edd8b46a011d
mconneely-sb <84188442+mconneely-sb@users.noreply.github.com>
2025-03-07 14:34:51 +0000

    Update ConsumerPactTests.cs

--- tests/ConsumerPactTests.cs
+++ tests/ConsumerPactTests.cs
@@ -48,7 +48,7 @@ namespace tests
                 .WithRequest(HttpMethod.Get, "/Products")
                 .WillRespond()
                 .WithStatus(System.Net.HttpStatusCode.OK)
-                .WithHeader("Content-Type", ""application/json; charset=utf-8")
+                .WithHeader("Content-Type", "application/json; charset=utf-8")
                 .WithJsonBody(Match.MinType(new
                 {
                     id = 27,
